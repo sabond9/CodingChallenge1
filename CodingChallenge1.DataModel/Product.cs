d79d7230635f399749ac06b68a1c478f6fa66b5e
Anton Bondarenko <anton.bondarenko@railsreactor.com>
2019-08-09 11:40:14 +0300

    Add project files.

---
+++ CodingChallenge1.DataModel/Product.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodingChallenge1.DataModel
+{
+    public class Product
+    {
+    }
+}
---
+++ CodingChallenge1/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingChallenge1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+    }
+}
